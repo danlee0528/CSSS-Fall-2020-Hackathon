63b1c1fb9a94c4f2de54f39d6ffb15a66a3103ab
danlee0528 <danlee0528@hotmail.com>
2020-11-07 15:45:07 -0800

    choose an option for the second step automatically

--- python_org_search.py
+++ python_org_search.py
@@ -14,19 +14,15 @@ driver.get(webLink)
 assert "Driver Licensing" in driver.title 
 
 # First Step: service option
-optionB = driver.find_element_by_id("da8488da9b5df26d32ca58c6d6a7973bedd5d98ad052d62b468d3b04b080ea25")
-driver.execute_script("arguments[0].click()", optionB)
+# choose the second option
+step1 = driver.find_element_by_id("da8488da9b5df26d32ca58c6d6a7973bedd5d98ad052d62b468d3b04b080ea25")
+driver.execute_script("arguments[0].click()", step1)
 
-# Second Step: location
-# location = driver.find_element_by_name("v-radio-79")
-
-# Thrid step: date
-
-
-
-
-
-
+# Second Step:
+# choose the first option
+driver.implicitly_wait(3)
+step2 = driver.find_element_by_css_selector("input[type='radio'][name='v-radio-79']")
+driver.execute_script("arguments[0].click()", step2)
 
 assert "No results found." not in driver.page_source
-driver.close()
+# driver.close()
