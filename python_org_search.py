ec1763f58cafa829952b609e67129d51f26041d7
Sukhwinder Singh <sukhwind@sfu.ca>
2020-11-07 16:31:04 -0800

    task 4 added

--- python_org_search.py
+++ python_org_search.py
@@ -24,5 +24,29 @@ driver.implicitly_wait(3)
 step2 = driver.find_element_by_css_selector("input[type='radio'][name='v-radio-79']")
 driver.execute_script("arguments[0].click()", step2)
 
+#Fourth step
+driver.implicitly_wait(5)
+last_name= driver.find_element_by_id('LastName')
+last_name.clear()
+last_name.send_keys("Singh")
+first_name= driver.find_element_by_id('FirstName')
+first_name.clear()
+first_name.send_keys("Sukhwinder")
+date_of_birth= driver.find_element_by_id('DOB')
+date_of_birth.clear()
+date_of_birth.send_keys("19990327")
+email= driver.find_element_by_id('Email')
+email.clear()
+email.send_keys("sukh.dhaliwal.9678@gmail.com")
+confirm_email= driver.find_element_by_id('ConfirmEmail')
+confirm_email.clear()
+confirm_email.send_keys("sukh.dhaliwal.9678@gmail.com")
+phone= driver.find_element_by_id('Phone')
+phone.clear()
+phone.send_keys('0000000000')
+
+
+
+
 assert "No results found." not in driver.page_source
 # driver.close()
